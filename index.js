701137d54f4c428633c79be929890c1d462ce691
sarthak572 <sarthakbansal2023@gmail.com>
2026-09-15 09:47:32 +0530

    added health checkpoint

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello from Dockerized Node.js Backend deployed via GitHub Actions locally!');
 });
+app.get('/health', (req, res) => {
+  res.send('health of this system is perfectly fine now ');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
