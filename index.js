05e22d9773521a8439e2ab413bd29cd110228744
sarthak572 <sarthakbansal2023@gmail.com>
2026-09-15 10:54:45 +0530

    added the

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js Backend!');
+  res.send('Hello from Docke   rized Node.js Backend!');
 });
 
 // A new route to test our environment variables / secrets
