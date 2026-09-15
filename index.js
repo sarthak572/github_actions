db459be76c9a870dae527f5ff59c8d92c64476d2
sarthak572 <sarthakbansal2023@gmail.com>
2026-09-15 09:56:28 +0530

    added the env data

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js Backend deployed via GitHub Actions locally!');
+  res.send('Hello from Dockerized Node.js Backend!');
 });
-app.get('/health', (req, res) => {
-  res.send('health of this system is perfectly fine now ');
+
+// A new route to test our environment variables / secrets
+app.get('/status', (req, res) => {
+  // We will inject this password securely via GitHub Secrets!
+  const dbPassword = process.env.DB_PASSWORD;
+  
+  if (dbPassword) {
+    res.send(`Secure backend is running! We connected to the database with password: ${dbPassword}`);
+  } else {
+    res.send('Warning: No DB_PASSWORD provided in environment variables! Server is running insecurely.');
+  }
 });
 
 app.listen(port, () => {
